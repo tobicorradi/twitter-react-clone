c5cb881a24a2baa73ae5662779316ce21a153229
tobicorradi <corraditobias@gmail.com>
2020-12-21 01:31:45 -0300

    Footer Created

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Website developed by Tobías Corradi - No commercial purporses intended,
+        made only for showing web development techniques and to be included as a
+        piece of portfolio. All copyrights are owned by Twitter.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
